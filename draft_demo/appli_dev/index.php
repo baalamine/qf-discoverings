fa0f56c456fb83900a981aa76983dd69dcda07be
mlba <ba@enst.fr>
2015-10-13 14:01:48 +0300

    create folder appli_dev

---
+++ draft_demo/appli_dev/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+<!-- Page header -->
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<title>Active Truth Discovering for Relevant Information Extraction</title>
+</head>
+<!-- Page Body -->
+<body>
+</body>
+<!-- header section -->
+<header class="body">
+</header>
+<!-- content section -->
+<section class="body">
+<form>
+        
+    <label>Name</label>
+    <input name="name" placeholder="Type Here">
+            
+    <label>Email</label>
+    <input name="email" type="email" placeholder="Type Here">
+            
+    <label>Message</label>
+    <textarea name="message" placeholder="Type Here"></textarea>
+            
+    <input id="submit" name="submit" type="submit" value="Submit">
+        
+</form>
+</section>
+<!-- footer section -->
+<footer class="body">
+</footer>
+</html>
